ed0116e5454061cd13512bf1e0363722939e89cc
xianzhe.xxz <xianzhe.xxz@alibaba-inc.com>
2023-05-08 14:36:57 +0800

    update scripts for train and eval

--- evaluate_swin.sh
+++ evaluate_swin.sh
@@ -1,2 +1,2 @@
-name='aline_swin-s_sota'
+name='lip_solider_swin_tiny'
 python evaluate.py --arch swin_small --data-dir /home/xianzhe.xxz/datasets/HumanParsing/LIP --model-restore ./logs/${name}/schp_4_checkpoint.pth.tar --input-size 572,384 --multi-scales 0.5,0.75,1.0,1.25,1.5 --flip
